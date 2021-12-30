aa62abaf1cc77c56c0cdd8b37eb84431cab11016
whiteadol <whiteadol3@naver.com>
2021-12-30 09:17:56 +0900

    first commit

---
+++ src/test/java/com/studysenior/callcenter/CallcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studysenior.callcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
